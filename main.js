c70744cd7c9dd83871c3fcc790a453b7b02995a7
Mst Joni Khatun <jmmonjurahman@gmail.com>
2026-07-22 21:55:08 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function submitData(){
+    let getnumber = document.getElementById('number').value;
+
+if (getnumber === ''){
+  return alert("field should not blank");
+ 
+}
+
+if(getnumber =='10' ){
+alert("match");
+
+}
+else{
+  alert("not match")
+}
+
+}
